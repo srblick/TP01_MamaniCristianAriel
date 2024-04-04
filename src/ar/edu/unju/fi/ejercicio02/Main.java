bc8af56f2ccb34404c58424e301f93b54e87cfe7
Cristian ARiel Mamani <sr.blick@gmail.com>
2024-04-04 16:39:59 -0300

    Se termino ejecicio 02.

---
+++ src/ar/edu/unju/fi/ejercicio02/Main.java
@@ -0,0 +1,24 @@
+package ar.edu.unju.fi.ejercicio02;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nombrePais = "Argentina";
+		short edad = 35;
+		float alturaEdificio = 110.5f;
+		float precio = 1499.99f;
+		long telefono = 3884600000l;
+		double coseno = Math.cos(0.5);
+	
+		System.out.println("Nombre del pais: " + nombrePais);
+		System.out.println("La edad es: " + edad);
+		System.out.println("Altura del edificio es: " + alturaEdificio);
+		System.out.println("Precio del producto: " + precio);
+		System.out.println("Telefono: " + telefono);
+		System.out.println("El Coseno de 0.5 es: " + coseno);
+
+
+	}
+
+}
