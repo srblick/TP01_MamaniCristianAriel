dd37e33dcfc2c7bf953f6f5ee6877a9377b91bb2
Cristian ARiel Mamani <sr.blick@gmail.com>
2024-04-04 16:09:24 -0300

    Ejercicio01

---
+++ src/ar/edu/unju/fi/ejercicio01/Main.java
@@ -0,0 +1,17 @@
+package ar.edu.unju.fi.ejercicio01;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numero1 = 8;
+		int numero2 = 5;
+		int numero3 = 1;
+		int numero4 = 20;
+		int numero5 = 9;
+		float promedio = (float) (numero1 + numero2 + numero3 + numero4 + numero5) / 5;
+		System.out.println(numero1 + " " + numero2 + " " + numero3 + " " + numero4 + " " + numero5);
+		System.out.println("El promedio es: " + promedio);
+	}
+
+}
